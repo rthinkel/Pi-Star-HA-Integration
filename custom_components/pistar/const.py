8a2f8e36f582817c62933c578fe89fb76f27d0b1
rt <55673663+rthinkel@users.noreply.github.com>
2026-08-22 19:23:37 -0400

    Define supported polling interval range

--- custom_components/pistar/const.py
+++ custom_components/pistar/const.py
@@ -1,8 +1,13 @@
+"""Constants for the Pi-Star integration."""
+
 DOMAIN = "pistar"
+
 DEFAULT_HOST = "pi-star.local"
 DEFAULT_USERNAME = "pi-star"
 DEFAULT_PASSWORD = "raspberry"
 DEFAULT_SCAN_INTERVAL = 30
+MIN_SCAN_INTERVAL = 10
+MAX_SCAN_INTERVAL = 3600
 
 CONF_HOST = "host"
 CONF_USERNAME = "username"
